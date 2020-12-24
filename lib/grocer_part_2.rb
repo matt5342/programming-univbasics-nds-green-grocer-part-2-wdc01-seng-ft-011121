98eea478171873888df957c5c0075d260810791c
Matthew Sewell <mattfsewell@gmail.com>
2020-12-24 23:31:15 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -46,7 +46,7 @@ def checkout(cart, coupons)
   cart.each do |item_hash|
     item_hash[:price].round(2)
     total += item_hash[:price] * item_hash[:count]
-    # total.round(2)
+    total.round(2)
     # binding.pry
   end
   if total > 100
