02b58030dbf25eab2d51e7e566137775adf67045
Matthew Sewell <mattfsewell@gmail.com>
2020-12-24 22:24:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,9 +47,9 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  consolidate_cart(cart)
-  apply_coupons(cart, coupons)
-  apply_clearance(cart)
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart =apply_clearance(cart)
   total = 0.0
   cart.each do |item_hash|
    
